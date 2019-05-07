773afcfa74bcba3814e6a9edf4cd7d40784f03e6
hfli <hfli@thoughtworks.com>
2019-05-07 09:24:18 +0800

    created ios swift plugin project

--- ios/Classes/SwiftEncryptionsPlugin.swift
+++ ios/Classes/SwiftEncryptionsPlugin.swift
@@ -9,6 +9,15 @@ public class SwiftEncryptionsPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    let bytes:[UInt8] = [1, 2, 3, 4, 5];
+    let data = NSData(bytes:bytes, length: 5);
+    switch call.method {
+    case "aesEncrypt","aesDecrypt","argon2i":
+        result(FlutterStandardTypedData(bytes: data as Data));
+    default:
+        result(FlutterMethodNotImplemented);
+    }
+    
     result("iOS " + UIDevice.current.systemVersion)
   }
 }
