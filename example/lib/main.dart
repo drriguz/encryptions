6e6b4e3092739ba4a33cd14b2615d74144f35617
hfli <hfli@thoughtworks.com>
2019-05-08 23:51:20 +0800

    refactor

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -22,30 +22,45 @@ class _MyAppState extends State<MyApp> {
     initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    const String key =
-        "dce60234d641f71f377ecafb5a566ce954d26c03fd3b5b23e9ed092ef42b5290";
-    const String iv = "c1f6fd873e14050697c168b3e9da5db2";
-    const String value = "01040000000300000002400000008B2E";
-    String result;
-    // Platform messages may fail, so we use a try/catch PlatformException.
+  Future<String> testAes(String method, String key, String iv, String value,
+      String expected) async {
+    var success = true;
     try {
       Uint8List encrypted = await Encryptions.aesEncryptInHex(key, iv, value);
       Uint8List decrypted = await Encryptions.aesDecrypt(
           Hex.decode(key), Hex.decode(iv), encrypted);
-      result = "Original:" +
-          value +
-          "\nEncrypted:" +
-          Hex.encode(encrypted) +
-          "\nDecrypted:" +
-          Hex.encode(decrypted);
 
-      Uint8List argon2i = await Encryptions.argon2i("password", "hello world!");
-      result += "\nArgon2i:" + Hex.encode(argon2i);
+      success =
+          Hex.encode(encrypted) == expected && Hex.encode(decrypted) == value;
     } on PlatformException {
-      result = 'Failed to get platform version.';
+      success = false;
     }
+    return method + ":" + (success ? "✔ " : "✗");
+  }
+
+  Future<String> testAes256CBCNoPadding() async {
+    const String key =
+        "dce60234d641f71f377ecafb5a566ce954d26c03fd3b5b23e9ed092ef42b5290";
+    const String iv = "c1f6fd873e14050697c168b3e9da5db2";
+    const String value = "01040000000300000002400000008b2e";
+    const String expectedEncrypted = "9a0106470245744f9121bbafa5dd10df";
+
+    return testAes("Aes256/CBC/NoPadding", key, iv, value, expectedEncrypted);
+  }
+
+  Future<String> testAes128CBCNoPadding() async {
+    const String key = "dce60234d641f71f377ecafb5a566ce9";
+    const String iv = "c1f6fd873e14050697c168b3e9da5db2";
+    const String value = "01040000000300000002400000008b2e";
+    const String expectedEncrypted = "64fb88a3f1a4d75d05c5508b2f2d4893";
+
+    return testAes("Aes128/CBC/NoPadding", key, iv, value, expectedEncrypted);
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    final String result =
+        await testAes256CBCNoPadding() + "\n" + await testAes128CBCNoPadding();
 
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
@@ -65,7 +80,7 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('AES Encryption Result:\n$_result\n'),
+          child: Text('Test result:\n$_result\n'),
         ),
       ),
     );
